cd6a3bd0cd6d3e8bf63c8a2f24de76a915a179f5
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-02 15:01:45 +0500

    food coordination with x or y

--- snake Game/script.js
+++ snake Game/script.js
@@ -1,52 +1,62 @@
-//In JavaScript, you represent the snake as an array of coordinate objects where the first element (snake[0]) is the head. Instead of shifting every body block simultaneously, you create a brand new head position based on your current direction, insert it at the very front of the array using .unshift(), and remove the very last segment of the tail using .pop(). This creates the illusion that the entire body is seamlessly slithering along the path
-
-//const snake = document.querySelectorAll(".snake");
-// const game = document.getElementById("game");
-const snakePosition = { x: 1, y: 1 };
-// const snakecoordinate = [snake[0],snake[1],snake[2],snake[3]];
-// function draw() {
-//   snake[0].style.gridColumnStart = snakePosition.x;
-//   snake[0].style.gridRowStart = snakePosition.y;
-// }
-
-// // const speed = setInterval(() => {
-// //   snakePosition.y += 1;
-
-// // }, 100);
-
-// window.addEventListener("keydown", function (e) {
-//   if (e.key === "ArrowUp") {
-//     console.log("up ");
-//     snakePosition.y > 1 ? snakePosition.y--  n: console.log("worng up ");
-//   } else if (e.key === "ArrowDown") {
-//     console.log("dowm");
-//     snakePosition.y ? snakePosition.y++ : console.log("down2");
-//   } else if (e.key === "ArrowLeft") {
-//     console.log("left");
-//     snakePosition.x > 1 ? snakePosition.x-- : console.log("left2");
-//   } else if (e.key === "ArrowRight") {
-//     console.log("right");
-//     snakePosition.x ? snakePosition.x++ : console.log("right");
-//   }
-//   draw();
-// });
-
-// draw();
-
+const snake = document.querySelectorAll(".snake");
+const food = document.querySelector(".food");
+const gameborad = document.getElementById("game");
+const snakePosition = { x: 0, y: 0 };
+let foodIntial = { x: 0, y: 0 };
 const snakeCoord = [
-  { x: 1, y: 1 },
-  { x: 1, y: 2 },
-  { x: 1, y: 3 },
+  { x: 1, y: 1 }, // head
+  { x: 2, y: 1 }, // body
+  { x: 3, y: 1 }, // tail
 ];
+
+/*const speed = setInterval(() => {
+  let y = 1;  
+  snake.forEach((s)=>{
+     s.style.gridRowStart = y++;
+  })
+}, 1000);
+*/
+
+function draw() {
+  snake.forEach((s, idx) => {
+    s.style.gridColumnStart = snakeCoord[idx].x;
+    s.style.gridRowStart = snakeCoord[idx].y;
+  });
+}
+
+function rm_num() {
+  const x = Math.floor(Math.random() * 9);
+  const y = Math.floor(Math.random() * 9);
+  foodIntial.x += x;
+  foodIntial.y += y;
+}
+
 const movesnake = (position) => {
+  food.style.gridColumnStart = foodIntial.x;
+  food.style.gridRowStart = foodIntial.y;
+
+  if (snakeCoord[0].x === foodIntial.x && snakeCoord[0].y === foodIntial.y) {
+       console.log(snakeCoord[0].x , snakeCoord[0].y , foodIntial.x , foodIntial.y)  
+              rm_num();
+    console.log("eating");
+  }
+
+   console.log(snakeCoord[0].x , snakeCoord[0].y , foodIntial.x , foodIntial.y)  
+
+
   if (position === "down") {
-    snakeCoord.unshift({ x: snakePosition.x, y: --snakePosition.y });
+    snakeCoord.unshift({ x: snakePosition.x, y: snakePosition.y++ });
     snakeCoord.pop();
   } else if (position === "right") {
     snakeCoord.unshift({ x: snakePosition.x++, y: snakePosition.y });
     snakeCoord.pop();
+  } else if (position === "up") {
+    snakeCoord.unshift({ x: snakePosition.x, y: snakePosition.y-- });
+    snakeCoord.pop();
+  } else if (position === "left") {
+    snakeCoord.unshift({ x: snakePosition.x--, y: snakePosition.y });
+    snakeCoord.pop();
   }
-  console.log(snakeCoord);
 };
 
 window.addEventListener("keydown", (e) => {
@@ -54,5 +64,7 @@ window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowDown") movesnake("down");
   if (e.key === "ArrowLeft") movesnake("left");
   if (e.key === "ArrowRight") movesnake("right");
+  draw();
 });
 
+draw();
