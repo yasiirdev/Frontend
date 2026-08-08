c2630c17828ebdd8ff6e1aa1cdd34f058616b993
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-08 21:36:11 +0500

    components , home , pages , auth

---
+++ ecowise/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-500">welcome to my react project of ecowise </div>
+    </>
+  );
+}
+
+export default App;
