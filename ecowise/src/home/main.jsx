0ba1708dc0121398e1c5caba236ba1794ac8e3aa
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-13 22:34:12 +0500

    vesionNO1 : main components that call footer,header,hero , etc

---
+++ ecowise/src/home/main.jsx
@@ -0,0 +1,14 @@
+import Footer from "./footer";
+import  Header  from "./header";
+import Hero from "./hero";
+import { Home } from "./Home";
+export function Main() {
+  return (
+    <>
+      <Header />
+      <Hero/>
+      <Home />
+      <Footer />
+    </>
+  );
+}
