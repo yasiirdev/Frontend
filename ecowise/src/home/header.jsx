fdad3b760596d5906f764be7a1a4913470189bb9
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-14 22:54:35 +0500

    @versionNO2 : give it a good shadow and logo

--- ecowise/src/home/header.jsx
+++ ecowise/src/home/header.jsx
@@ -1,3 +1,4 @@
+import logo from "../assets/logo.svg";
 const navLinks = [
   { nav: "Home", path: "men" },
   { nav: "Features", path: "men" },
@@ -6,10 +7,13 @@ const navLinks = [
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-40 border-b shadow-2xl border-white bg-[#F1F8E9]/80 backdrop-blur w-screen">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3  h-12 bg-[#F1F8E9]">
+    <header className="sticky top-0 z-40 border-b font-Inter  bg-main  w-screen  backdrop-blur-sm border border-white/50  shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)]    before:absolute before:inset-0 before:rounded-lg after:bg-linear-to-tl before:bg-linear-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0  after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3  h-12">
         <div className="flex items-center gap-70">
-          <a className="text-xl font-extrabold  text-[#185E20]">EcoWise</a>
+          <a className="text-xl font-extrabold flex font-Inter items-center gap-2 text-bg-primary"> 
+            <img src={logo} alt="ecowise logo" className="h-8 w-8" />
+            EcoWise
+          </a>
           <nav className="hidden items-center gap-5 text-sm capitalize text-gray-800 md:flex">
             {navLinks.map((item) => {
               return (
