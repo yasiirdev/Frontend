0a02bba06a3867d752e1813452aa242c97479196
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-14 23:01:33 +0500

    @versionNo1 : feature cards with liquit glass

---
+++ ecowise/src/home/page2.jsx
@@ -0,0 +1,39 @@
+export default function Page2() {
+  return (
+    <div className="w-screen h-screen pt-10 bg-main">
+      <div className="flex items-center gap-8 justify-center w-screen min-h-100 bg-cover bg-center bg-no-repeat rounded-md ">
+        <div className="max-w-sm  backdrop-blur-sm border border-white/50 rounded-lg shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] p-6  relative before:absolute before:inset-0 before:rounded-lg after:bg-linear-to-tl before:bg-linear-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none">
+          <div className="relative z-10">
+            <div className="flex flex-col items-center text-center">
+              <div className="w-12 h-12 rounded-full flex items-center justify-center mb-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-green-300">
+                        <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                      </svg>
+                    </div>
+                    <h3 class="text-lg font-Mon text-green-900 font-medium mb-2">Carbon Footprint Tracking</h3>
+              <p class="text-sm font-Inter opacity-90 mb-4 leading-relaxed">
+          We calculate the lifetime carbon emissions of every product, from raw material extraction to final delivery.
+              </p>
+                  </div>
+                </div>
+              </div>
+{/*second container */}
+<div className="max-w-sm  backdrop-blur-sm border border-white/50 rounded-lg shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] p-6  relative before:absolute before:inset-0 before:rounded-lg after:bg-linear-to-tl before:bg-linear-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none">
+          <div className="relative z-10">
+            <div className="flex flex-col items-center text-center">
+              <div className="w-12 h-12 rounded-full flex items-center justify-center mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-green-300">
+                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+              </svg>
+            </div>
+            <h3 class="text-lg text-green-900 font-Mon font-medium mb-2">Real-time Eco-Scoring</h3>
+            <p class="text-sm font-Inter opacity-90 mb-4 leading-relaxed">Unlock advanced capabilities with our premium tier. Get access to exclusive tools and enhanced performance.</p>
+          </div>
+        </div>
+      </div>
+        
+    </div>
+  
+  </div>
+  );
+}
