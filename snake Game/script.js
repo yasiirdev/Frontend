6fb43339938e184fb427ca0853b371aa74eef2ff
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-08-24 14:37:24 +0500

    @verion4 : writing a clean code using classes

--- snake Game/script.js
+++ snake Game/script.js
@@ -1,149 +1,167 @@
-let _DIR = { x: 0, y: 1 };
-const GRID = 13;
-let snake = [
-  { x: 3, y: 1 },
-  { x: 3, y: 2 },
-  { x: 3, y: 3 },
-];
-// food state
-let foodDir = { x: 3, y: 5 };
-let interval = null;
+class snakeGame {
+  constructor() {
+    this.eventStart();
+    this.direction = { x: 0, y: 1 };
+    this.GRID = 13;
+    this.snakebody = [
+      { x: 3, y: 1 },
+      { x: 3, y: 2 },
+      { x: 3, y: 3 },
+    ];
+    this.foodState = { x: 3, y: 5 };
+    this.interval = null;
+    this.gameboard = document.querySelector(".gameboard");
+    this.startbtu = document.querySelector(".start");
+    this.normalbtu = document.querySelector(".normal");
+    this.mediumbtu = document.querySelector(".medium");
+    this.hardbtu = document.querySelector(".hard");
+    this.buttonFunctionality();
+  } 
 
-const main = () => {
-  update();
-  draw();
-  checkClearState();
-};
-
-function clear() {
-  clearInterval(interval);
-  document.querySelector(".controler").style.display = "flex";
-  document.querySelector(".gameboard").innerHTML = "";
-  document.querySelector(".gameboard").classList.add("gameover");
-  const gameOver = document.createElement("h1");
-  gameOver.innerText = "Game Over";
-  gameOver.classList.add("boardText");
-  document.querySelector(".gameboard").appendChild(gameOver);
-
-  document.querySelector(".normal").style.display = "none";
-  document.querySelector(".medium").style.display = "none";
-  document.querySelector(".hard").style.display = "none";
-  document.querySelector(".start").style.display = "block";
-}
-
-function checkClearState() {
-  if (snake[3]) {
-    for (let i = 3; i <= snake.length; i++) {
-      let elem = snake[i];
-      snake[0].x == elem?.x && snake[0].y == elem?.y ? clear() : null;
-    }
+  main() {
+    this.update();
+    this.draw();
+    this.foodIn();
+    this.checkClearState();
   }
 
-  snake[0].y > GRID || snake[0].x > GRID || snake[0].y <= 0 || snake[0].x <= 0 ? clear() : null;
-}
+  update() {
+    let newhead = {
+      x: this.snakebody[0].x + this.direction.x,
+      y: this.snakebody[0].y + this.direction.y,
+    };
 
-function update() {
-  let newhead = {
-    x: snake[0].x + _DIR.x,
-    y: snake[0].y + _DIR.y,
-  };
-  snake.unshift(newhead);
-  snake.pop();
-}
+    this.snakebody.unshift(newhead);
+    this.snakebody.pop();
+  }
 
-const foodIn = () => {
-  const food = document.createElement("img");
-  food.classList.add("food");
-  document.querySelector(".gameboard").appendChild(food);
+  draw() {
+    this.gameboard.innerHTML = "";
+    this.gameboard.classList.add("gameboard");
+    this.snakebody.forEach((packet) => {
+      const snakeElement = document.createElement("div");
+      snakeElement.style.gridColumnStart = packet.x;
+      snakeElement.style.gridRowStart = packet.y;
+      snakeElement.classList.add("snake");
+      this.gameboard.appendChild(snakeElement);
+    });
+  }
 
-  // set the state value
-  food.style.gridColumnStart = foodDir.x;
-  food.style.gridRowStart = foodDir.y;
+  foodIn() {
+    const food = document.createElement("img");
+    food.classList.add("food");
+    this.gameboard.appendChild(food);
 
-  if (snake[0].x === foodDir.x && snake[0].y === foodDir.y) {
-    // start
-    const x_random = Math.round(Math.random() * GRID);
-    const y_random = Math.round(Math.random() * GRID);
+    food.style.gridColumnStart = this.foodState.x;
+    food.style.gridRowStart = this.foodState.y;
 
-    // loop start here
-    if (x_random !== 0 && y_random !== 0) {
-      foodDir.x = x_random;
-      foodDir.y = y_random;
+    if (this.snakebody[0].x === this.foodState.x && this.snakebody[0].y === this.foodState.y) {
+      const x_random = Math.round(Math.random() * this.GRID);
+      const y_random = Math.round(Math.random() * this.GRID);
+
+      if (x_random !== 0 && y_random !== 0) {
+        this.foodState.x = x_random;
+        this.foodState.y = y_random;
+      }
+      this.snakebody.unshift({
+        x: Number(food.style.gridColumnStart),
+        y: Number(food.style.gridRowStart),
+      });
+    } 
+  }
+
+  checkClearState() {
+    if (this.snakebody[3]) {
+      for (let i = 3; i <= this.snakebody.length; i++) {
+        let elem = this.snakebody[i];
+        this.snakebody[0].x == elem?.x && this.snakebody[0].y == elem?.y ? this.clear() : null;
+      }
     }
 
-    snake.unshift({ x: Number(food.style.gridColumnStart), y: Number(food.style.gridRowStart) });
-  } // end
-};
+    this.snakebody[0].y > this.GRID ||
+    this.snakebody[0].x > this.GRID ||
+    this.snakebody[0].y <= 0 ||
+    this.snakebody[0].x <= 0
+      ? this.clear()
+      : null;
+  }
 
-function draw() {
-  const gameboard = document.querySelector(".gameboard");
-  gameboard.innerHTML = "";
-  gameboard.classList.add("gameboard");
-  snake.forEach((packet) => {
-    const snakeElement = document.createElement("div");
-    snakeElement.style.gridColumnStart = packet.x;
-    snakeElement.style.gridRowStart = packet.y;
-    snakeElement.classList.add("snake");
-    gameboard.appendChild(snakeElement);
-  });
+  clear() {
+    clearInterval(this.interval);
+    document.querySelector(".controler").style.display = "flex";
+    this.gameboard.innerHTML = "";
+    this.gameboard.classList.add("gameover");
+    const gameOverHeading = document.createElement("h1");
+    gameOverHeading.innerText = "Game Over";
+    gameOverHeading.classList.add("boardText");
+    this.gameboard.appendChild(gameOverHeading);
 
-  foodIn();
-}
+    this.normalbtu.style.display = "none";
+    this.mediumbtu.style.display = "none";
+    this.hardbtu.style.display = "none";
+    this.startbtu.style.display = "block";
+  }
 
-const eventStart = () => {
-  let checkdir = "down";
-  document.addEventListener("keydown", (e) => {
-    e.stopPropagation();
-    if (e.key === "ArrowUp" && checkdir !== "down") {
-      checkdir = "up";
-      _DIR.y = -1;
-      _DIR.x = 0;
-    } else if (e.key === "ArrowRight" && checkdir !== "left") {
-      checkdir = "right";
-      _DIR.x = 1;
-      _DIR.y = 0;
-    } else if (e.key === "ArrowLeft" && checkdir !== "right") {
-      checkdir = "left";
-      _DIR.x = -1;
-      _DIR.y = 0;
-    } else if (e.key === "ArrowDown" && checkdir !== "up") {
-      checkdir = "down";
-      _DIR.y = 1;
-      _DIR.x = 0;
-    }
-  });
-};
+  buttonFunctionality() {
+    this.startbtu.style.display = "none";
 
-// DOMContentLoaded
+    this.startbtu.addEventListener("click", (e) => {
+      history.go(0);
+      e.stopPropagation();
+      this.startbtu.style.display = "none";
+      this.normalbtu.style.display = "block";
+      this.mediumbtu.style.display = "block";
+      this.hardbtu.style.display = "block";
+    });
+
+    this.normalbtu.addEventListener("click", (e) => {
+      e.stopPropagation();
+      document.querySelector(".controler").style.display = "none";
+      this.interval = setInterval(() => this.main(), 250);
+    });
+
+    this.mediumbtu.addEventListener("click", (e) => {
+      e.stopPropagation();
+      document.querySelector(".controler").style.display = "none";
+      this.interval = setInterval(() => this.main(), 200);
+    });
+
+    this.hardbtu.addEventListener("click", (e) => {
+      e.stopPropagation();
+      document.querySelector(".controler").style.display = "none";
+      this.interval = setInterval(() => this.main(), 100);
+    });
+  }
+
+  eventStart() {
+    let checkdir = "down";
+    document.addEventListener("keydown", (e) => {
+      e.stopPropagation();
+      if (e.key === "ArrowUp" && checkdir !== "down") {
+        checkdir = "up";
+        this.direction.y = -1;
+        this.direction.x = 0;
+      } else if (e.key === "ArrowRight" && checkdir !== "left") {
+        checkdir = "right";
+        this.direction.x = 1;
+        this.direction.y = 0;
+      } else if (e.key === "ArrowLeft" && checkdir !== "right") {
+        checkdir = "left";
+        this.direction.x = -1;
+        this.direction.y = 0;
+      } else if (e.key === "ArrowDown" && checkdir !== "up") {
+        checkdir = "down";
+        this.direction.y = 1;
+        this.direction.x = 0;
+      }
+    }); 
+  } 
+} 
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  eventStart();
-
-  document.querySelector(".start").style.display = "none";
-
-  document.querySelector(".start").addEventListener("click", (e) => {
-    history.go(0);
-    e.stopPropagation();
-    document.querySelector(".start").style.display = "none";
-    document.querySelector(".normal").style.display = "block";
-    document.querySelector(".medium").style.display = "block";
-    document.querySelector(".hard").style.display = "block";
-  });
-
-  document.querySelector(".normal").addEventListener("click", (e) => {
-    e.stopPropagation();
-    document.querySelector(".controler").style.display = "none";
-    interval = setInterval(main, 250);
-  });
-
-  document.querySelector(".medium").addEventListener("click", (e) => {
-    e.stopPropagation();
-    document.querySelector(".controler").style.display = "none";
-    interval = setInterval(main, 150);
-  });
-
-  document.querySelector(".hard").addEventListener("click", (e) => {
-    e.stopPropagation();
-    document.querySelector(".controler").style.display = "none";
-    interval = setInterval(main, 100);
-  });
+  new snakeGame();
 });
